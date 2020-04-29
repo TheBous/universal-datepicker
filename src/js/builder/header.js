ad80c15b6d29d6c47dce7404e8721d1cab3db69b
Valsecchi Luca <luca.valsecchi@hotelscan.com>
2020-04-29 14:31:10 +0200

    chore(todo): remove all todos

--- src/js/builder/header.js
+++ src/js/builder/header.js
@@ -1,5 +1,4 @@
 export const renderHeader = (month, monthsLabels, year) => {
-  // TODO add arrows (right - left)
   const currentMonth = monthsLabels[month];
   const header = document.createElement("div");
   header.className = "calendar__header";
