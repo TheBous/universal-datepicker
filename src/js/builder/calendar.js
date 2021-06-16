de1b0b36ebd7b3cfd3d425bea03c1f0ef7acc875
Luca Valsecchi <luca.valsecchi@hotelscan.com>
2021-06-16 10:32:27 +0200

    fix(var): add new var to calendar.js

--- src/js/builder/calendar.js
+++ src/js/builder/calendar.js
@@ -14,6 +14,7 @@ export const renderCalendarBlocks = ({
   monthsLabels,
   onCellClick,
   maxCheckin,
+  showTodayDate,
 }) => {
   const tableWrapper = document.createElement("div");
   tableWrapper.classList.add("table-wrapper");
